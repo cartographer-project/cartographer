15cf5ba1956437ce3a0de192a2c29c5c63753c15
Andrew Somerville <asomerville@carnegierobotics.com>
2016-10-10 12:02:02 -0400

    clang-format -i -style=Google cartographer/transform/transform.h

--- cartographer/transform/transform.h
+++ cartographer/transform/transform.h
@@ -62,12 +62,15 @@ Eigen::Matrix<T, 3, 1> RotationQuaternionToAngleAxisVector(
     normalized_quaternion.y() *= T(-1.);
     normalized_quaternion.z() *= T(-1.);
   }
-  // We convert the normalized_quaternion into a vector along the rotation axis with
+  // We convert the normalized_quaternion into a vector along the rotation axis
+  // with
   // length of the rotation angle.
-  const T angle = T(2.) * atan2(normalized_quaternion.vec().norm(), normalized_quaternion.w());
+  const T angle = T(2.) * atan2(normalized_quaternion.vec().norm(),
+                                normalized_quaternion.w());
   constexpr double kCutoffAngle = 1e-7;  // We linearize below this angle.
   const T scale = angle < kCutoffAngle ? T(2.) : angle / sin(angle / T(2.));
-  return Eigen::Matrix<T, 3, 1>(scale * normalized_quaternion.x(), scale * normalized_quaternion.y(),
+  return Eigen::Matrix<T, 3, 1>(scale * normalized_quaternion.x(),
+                                scale * normalized_quaternion.y(),
                                 scale * normalized_quaternion.z());
 }
 
