5f27fa669f5323f4c11130734f4a9b6bfc567de1
Holger Rapp <hrapp@google.com>
2016-10-11 13:41:50 +0200

    Adds a points processing pipeline and a XRay creator.
    - Adds cairo as required dependency.
    - Adds an 'io' module.

---
+++ cartographer/io/points_batch.h
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2016 The Cartographer Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef CARTOGRAPHER_IO_POINTS_BATCH_H_
+#define CARTOGRAPHER_IO_POINTS_BATCH_H_
+
+#include <cstdint>
+
+#include "Eigen/Core"
+#include "cartographer/common/time.h"
+
+namespace cartographer {
+namespace io {
+
+// A number of points, captured around the same 'time' and by a
+// sensor at the same 'origin'.
+struct PointsBatch {
+  PointsBatch() {
+    origin = Eigen::Vector3f::Zero();
+    trajectory_index = 0;
+  }
+
+  // Time at which this batch has been acquired.
+  common::Time time;
+
+  // Origin of the data, i.e. the location of the sensor in the world at
+  // 'time'.
+  Eigen::Vector3f origin;
+
+  // Sensor that generated this data's 'frame_id' or empty if this information
+  // is unknown.
+  string frame_id;
+
+  // Trajectory index that produced this point.
+  int trajectory_index;
+
+  std::vector<Eigen::Vector3f> points;
+  std::vector<Eigen::Vector3f> normals;
+  std::vector<Eigen::Matrix<uint8_t, 3, 1>> colors;
+};
+
+}  // namespace io
+}  // namespace cartographer
+
+#endif  // CARTOGRAPHER_IO_POINTS_BATCH_H_
