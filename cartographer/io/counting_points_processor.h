12dfc870b150723465febc19ed69c6515b068768
Holger Rapp <hrapp@google.com>
2016-10-25 15:41:51 +0200

    Adds a CountingPointsProcessor.
    It outputs the number of points processed at its stage in the pipeline
    and is useful for understanding how many points a filter removed.

--- cartographer/io/counting_points_processor.h
+++ cartographer/io/counting_points_processor.h
@@ -14,7 +14,8 @@
  * limitations under the License.
  */
 
-#include <fstream>
+#ifndef CARTOGRAPHER_IO_COUNTING_POINTS_PROCESSOR_H_
+#define CARTOGRAPHER_IO_COUNTING_POINTS_PROCESSOR_H_
 
 #include "cartographer/common/lua_parameter_dictionary.h"
 #include "cartographer/io/points_processor.h"
@@ -22,31 +23,30 @@
 namespace cartographer {
 namespace io {
 
-// Streams a PLY file to disk. The header is written in 'Flush'.
-class PlyWritingPointsProcessor : public PointsProcessor {
+// Passes through points, but keeps track of how many points it saw and output
+// that on Flush.
+class CountingPointsProcessor : public PointsProcessor {
  public:
-  constexpr static const char* kConfigurationFileActionName = "write_ply";
-  PlyWritingPointsProcessor(const string& filename, PointsProcessor* next);
+  constexpr static const char* kConfigurationFileActionName = "dump_num_points";
+  explicit CountingPointsProcessor(PointsProcessor* next);
 
-  static std::unique_ptr<PlyWritingPointsProcessor> FromDictionary(
+  static std::unique_ptr<CountingPointsProcessor> FromDictionary(
       common::LuaParameterDictionary* dictionary, PointsProcessor* next);
 
-  ~PlyWritingPointsProcessor() override {}
+  ~CountingPointsProcessor() override {}
 
-  PlyWritingPointsProcessor(const PlyWritingPointsProcessor&) = delete;
-  PlyWritingPointsProcessor& operator=(const PlyWritingPointsProcessor&) =
-      delete;
+  CountingPointsProcessor(const CountingPointsProcessor&) = delete;
+  CountingPointsProcessor& operator=(const CountingPointsProcessor&) = delete;
 
-  void Process(std::unique_ptr<PointsBatch> batch) override;
+  void Process(std::unique_ptr<PointsBatch> points) override;
   FlushResult Flush() override;
 
  private:
-  PointsProcessor* const next_;
-
   int64 num_points_;
-  bool has_colors_;
-  std::ofstream file_;
+  PointsProcessor* next_;
 };
 
 }  // namespace io
 }  // namespace cartographer
+
+#endif  // CARTOGRAPHER_IO_COUNTING_POINTS_PROCESSOR_H_
