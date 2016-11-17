8438ace641c1dd7fdce249764605333f0b3df2f0
Damon Kohler <damonkohler@google.com>
2016-11-17 10:33:36 +0100

    Replace USES_ROS through Catkin auto-detection.

--- scripts/update_cmakelists.py
+++ scripts/update_cmakelists.py
@@ -107,10 +107,6 @@ def ExtractUses(project_name, source):
       uses.add("USES_WEBP")
     if re.match(r'^#include ["<]pcl/', line):
       uses.add("USES_PCL")
-    if re.match(r'^#include ["<]ros/', line):
-      uses.add("USES_ROS")
-    if re.match(r'^#include "[a-zA-Z]*_msgs/', line):
-      uses.add("USES_ROS")
     if re.match(r'^#include ["<]yaml-cpp/', line):
       uses.add("USES_YAMLCPP")
     if re.match(r'^#include ["<]cairo/', line):
@@ -248,8 +244,6 @@ def RunOnDirectory(root):
             continue
           target.depends.add(dependant.name)
         target.uses.update(ExtractUses(project_name, src))
-      if target.type is "google_test" and "USES_ROS" in target.uses:
-        target.type = "google_catkin_test"
 
     cmake_file = path.join(directory, "CMakeLists.txt")
     parts = GetNonGoogleTargetLines(cmake_file)
