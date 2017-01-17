92b89d12c8c2746e010fd359a73b425a8c6197b4
Holger Rapp <HolgerRapp@gmx.net>
2017-01-17 17:21:05 +0100

    Adds a PointCloudProcessor that colors points by frame_id. (#184)

---
+++ cartographer/io/coloring_points_processor.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2016 The Cartographer Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef CARTOGRAPHER_IO_COLORING_POINTS_PROCESSOR_H_
+#define CARTOGRAPHER_IO_COLORING_POINTS_PROCESSOR_H_
+
+#include <memory>
+
+#include "cartographer/common/lua_parameter_dictionary.h"
+#include "cartographer/io/points_batch.h"
+#include "cartographer/io/points_processor.h"
+
+namespace cartographer {
+namespace io {
+
+// Colors points with a fixed color by frame_id.
+class ColoringPointsProcessor : public PointsProcessor {
+ public:
+  constexpr static const char* kConfigurationFileActionName = "color_points";
+
+  ColoringPointsProcessor(const Color& color, const string& frame_id,
+                          PointsProcessor* next);
+
+  static std::unique_ptr<ColoringPointsProcessor> FromDictionary(
+      common::LuaParameterDictionary* dictionary, PointsProcessor* next);
+
+  ~ColoringPointsProcessor() override{};
+
+  ColoringPointsProcessor(const ColoringPointsProcessor&) = delete;
+  ColoringPointsProcessor& operator=(const ColoringPointsProcessor&) = delete;
+
+  void Process(std::unique_ptr<PointsBatch> batch) override;
+  FlushResult Flush() override;
+
+ private:
+  const Color color_;
+  const string frame_id_;
+  PointsProcessor* const next_;
+};
+
+}  // namespace io
+}  // namespace cartographer
+
+#endif  // CARTOGRAPHER_IO_COLORING_POINTS_PROCESSOR_H_
