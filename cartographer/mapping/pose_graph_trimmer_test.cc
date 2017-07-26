30f7de1a325d6604c780f2f74d9a345ec369d12d
Jihoon Lee <jihoonlee.in@gmail.com>
2017-07-26 11:14:22 +0200

    fix num submaps in trimmer_test (#424)

--- cartographer/mapping/pose_graph_trimmer_test.cc
+++ cartographer/mapping/pose_graph_trimmer_test.cc
@@ -29,7 +29,7 @@ class FakePoseGraph : public Trimmable {
  public:
   ~FakePoseGraph() override {}
 
-  int num_submaps(int trajectory_id) const override { return 17; }
+  int num_submaps(int trajectory_id) const override { return 17 - trimmed_submaps_.size(); }
 
   void MarkSubmapAsTrimmed(const SubmapId& submap_id) override {
     trimmed_submaps_.push_back(submap_id);
