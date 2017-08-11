028d8d88f9b99dadc263f2d2096c54638aa697e4
Wolfgang Hess <whess@google.com>
2017-08-11 14:47:38 +0200

    Remove unused code and option for odometry states.

--- cartographer/mapping/map_builder.cc
+++ cartographer/mapping/map_builder.cc
@@ -199,10 +199,7 @@ void MapBuilder::LoadMap(io::ProtoStreamReader* const reader) {
   unused_options.mutable_trajectory_builder_2d_options()
       ->mutable_submaps_options()
       ->set_resolution(0.05);
-  unused_options.mutable_trajectory_builder_2d_options()
-      ->set_num_odometry_states(1);
-  unused_options.mutable_trajectory_builder_3d_options()
-      ->set_num_odometry_states(1);
+  unused_options.mutable_trajectory_builder_3d_options();
 
   const std::unordered_set<string> unused_sensor_ids;
   const int map_trajectory_id =
--- cartographer/mapping_2d/local_trajectory_builder_options.cc
+++ cartographer/mapping_2d/local_trajectory_builder_options.cc
@@ -56,9 +56,6 @@ proto::LocalTrajectoryBuilderOptions CreateLocalTrajectoryBuilderOptions(
           parameter_dictionary->GetDictionary("motion_filter").get());
   options.set_imu_gravity_time_constant(
       parameter_dictionary->GetDouble("imu_gravity_time_constant"));
-  options.set_num_odometry_states(
-      parameter_dictionary->GetNonNegativeInt("num_odometry_states"));
-  CHECK_GT(options.num_odometry_states(), 0);
   *options.mutable_submaps_options() = CreateSubmapsOptions(
       parameter_dictionary->GetDictionary("submaps").get());
   options.set_use_imu_data(parameter_dictionary->GetBool("use_imu_data"));
