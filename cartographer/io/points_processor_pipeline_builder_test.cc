cartographer-project/cartographer

commit ba6f782949eb7cdb96e43c178c7b6ef83bd57215

Fix ClangTidy warnings. (#455)

cartographer/mapping_3d/sparse_pose_graph/constraint_builder.h
@@ -103,7 +103,7 @@ class ConstraintBuilder {
 
   // Registers the 'callback' to be called with the results, after all
   // computations triggered by MaybeAddConstraint() have finished.
-  void WhenDone(std::function<void(const Result&)> callback);
+  void WhenDone(const std::function<void(const Result&)>& callback);
 
   // Returns the number of consecutive finished scans.
   int GetNumFinishedScans();
@@ -121,7 +121,7 @@ class ConstraintBuilder {
   void ScheduleSubmapScanMatcherConstructionAndQueueWorkItem(
       const mapping::SubmapId& submap_id,
       const std::vector<mapping::TrajectoryNode>& submap_nodes,
-      const Submap* submap, std::function<void()> work_item) REQUIRES(mutex_);
+      const Submap* submap, const std::function<void()>& work_item) REQUIRES(mutex_);
 
   // Constructs the scan matcher for a 'submap', then schedules its work items.
   void ConstructSubmapScanMatcher(

cartographer/mapping_2d/sparse_pose_graph/constraint_builder.h
@@ -102,7 +102,7 @@ class ConstraintBuilder {
 
   // Registers the 'callback' to be called with the results, after all
   // computations triggered by MaybeAddConstraint() have finished.
-  void WhenDone(std::function<void(const Result&)> callback);
+  void WhenDone(const std::function<void(const Result&)>& callback);
 
   // Returns the number of consecutive finished scans.
   int GetNumFinishedScans();
@@ -121,7 +121,7 @@ class ConstraintBuilder {
   // construction and queues the 'work_item'.
   void ScheduleSubmapScanMatcherConstructionAndQueueWorkItem(
       const mapping::SubmapId& submap_id, const ProbabilityGrid* submap,
-      std::function<void()> work_item) REQUIRES(mutex_);
+      const std::function<void()>& work_item) REQUIRES(mutex_);
 
   // Constructs the scan matcher for a 'submap', then schedules its work items.
   void ConstructSubmapScanMatcher(const mapping::SubmapId& submap_id,

cartographer/common/thread_pool.cc
@@ -45,7 +45,7 @@ ThreadPool::~ThreadPool() {
   }
 }
 
-void ThreadPool::Schedule(std::function<void()> work_item) {
+void ThreadPool::Schedule(const std::function<void()>& work_item) {
   MutexLocker locker(&mutex_);
   CHECK(running_);
   work_queue_.push_back(work_item);

cartographer/common/thread_pool.h
@@ -40,7 +40,7 @@ class ThreadPool {
   ThreadPool(const ThreadPool&) = delete;
   ThreadPool& operator=(const ThreadPool&) = delete;
 
-  void Schedule(std::function<void()> work_item);
+  void Schedule(const std::function<void()>& work_item);
 
  private:
   void DoWork();

cartographer/io/hybrid_grid_points_processor.cc
@@ -28,7 +28,7 @@ HybridGridPointsProcessor::HybridGridPointsProcessor(
 
 std::unique_ptr<HybridGridPointsProcessor>
 HybridGridPointsProcessor::FromDictionary(
-    FileWriterFactory file_writer_factory,
+    const FileWriterFactory& file_writer_factory,
     common::LuaParameterDictionary* const dictionary,
     PointsProcessor* const next) {
   return common::make_unique<HybridGridPointsProcessor>(

cartographer/io/hybrid_grid_points_processor.h
@@ -33,7 +33,7 @@ class HybridGridPointsProcessor : public PointsProcessor {
       delete;
 
   static std::unique_ptr<HybridGridPointsProcessor> FromDictionary(
-      FileWriterFactory file_writer_factory,
+      const FileWriterFactory& file_writer_factory,
       common::LuaParameterDictionary* dictionary, PointsProcessor* next);
 
   ~HybridGridPointsProcessor() override {}

cartographer/io/outlier_removing_points_processor.cc
@@ -41,18 +41,18 @@ OutlierRemovingPointsProcessor::OutlierRemovingPointsProcessor(
 }
 
 void OutlierRemovingPointsProcessor::Process(
-    std::unique_ptr<PointsBatch> points) {
+    std::unique_ptr<PointsBatch> batch) {
   switch (state_) {
     case State::kPhase1:
-      ProcessInPhaseOne(*points);
+      ProcessInPhaseOne(*batch);
       break;
 
     case State::kPhase2:
-      ProcessInPhaseTwo(*points);
+      ProcessInPhaseTwo(*batch);
       break;
 
     case State::kPhase3:
-      ProcessInPhaseThree(std::move(points));
+      ProcessInPhaseThree(std::move(batch));
       break;
   }
 }

cartographer/io/ply_writing_points_processor.cc
@@ -71,7 +71,7 @@ void WriteBinaryPlyPointColor(const Uint8Color& color,
 
 std::unique_ptr<PlyWritingPointsProcessor>
 PlyWritingPointsProcessor::FromDictionary(
-    FileWriterFactory file_writer_factory,
+    const FileWriterFactory& file_writer_factory,
     common::LuaParameterDictionary* const dictionary,
     PointsProcessor* const next) {
   return common::make_unique<PlyWritingPointsProcessor>(

commit 65b81725803a2e302fb3ad227bd3c03b464215fa

Fix header.
RegisterBuiltInPointsProcessors() declaration was broken in #455.

cartographer/io/points_processor_pipeline_builder.h
@@ -62,7 +62,7 @@ class PointsProcessorPipelineBuilder {
 // 'builder'.
 void RegisterBuiltInPointsProcessors(
     const std::vector<mapping::proto::Trajectory>& trajectories,
-    FileWriterFactory file_writer_factory,
+    const FileWriterFactory& file_writer_factory,
     PointsProcessorPipelineBuilder* builder);
 
 }  // namespace io

cartographer/mapping_3d/sparse_pose_graph/constraint_builder.h
@@ -121,7 +121,8 @@ class ConstraintBuilder {
   void ScheduleSubmapScanMatcherConstructionAndQueueWorkItem(
       const mapping::SubmapId& submap_id,
       const std::vector<mapping::TrajectoryNode>& submap_nodes,
-      const Submap* submap, const std::function<void()>& work_item) REQUIRES(mutex_);
+      const Submap* submap, const std::function<void()>& work_item)
+      REQUIRES(mutex_);
 
   // Constructs the scan matcher for a 'submap', then schedules its work items.
   void ConstructSubmapScanMatcher(

cartographer/io/points_processor_pipeline_builder_test.cc
@@ -1,0 +1,40 @@
+/*
+ * Copyright 2017 The Cartographer Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "cartographer/io/points_processor_pipeline_builder.h"
+
+#include <string>
+
+#include "cartographer/common/port.h"
+#include "cartographer/io/file_writer.h"
+#include "gtest/gtest.h"
+
+namespace cartographer {
+namespace io {
+namespace {
+
+TEST(PointsProcessorPipelineBuilderTest, RegisterBuiltInPointsProcessors) {
+  PointsProcessorPipelineBuilder builder;
+  FileWriterFactory dummy_factory =
+      [](const string& filename) -> std::unique_ptr<FileWriter> {
+    return nullptr;
+  };
+  RegisterBuiltInPointsProcessors({}, dummy_factory, &builder);
+}
+
+}  // namespace
+}  // namespace io
+}  // namespace cartographer