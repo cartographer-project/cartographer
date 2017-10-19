49ead6055c496f2b1eb67aaeea6ca3506f677e6b
Holger Rapp <HolgerRapp@gmx.net>
2017-10-19 12:30:19 +0200

    Towards a ROS map writing PointsProcessor. (#598)
    - Refactor ProbabilityGridPointsProcessor for code reuse.
    - Adds Image::width and height.
    - Adds GetFilename for FileWriter.
    
    Related to googlecartographer/cartographer_ros#475.

--- cartographer/io/image.h
+++ cartographer/io/image.h
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 The Cartographer Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #ifndef CARTOGRAPHER_IO_IMAGE_H_
 #define CARTOGRAPHER_IO_IMAGE_H_
 
@@ -40,6 +56,9 @@ class Image {
   // is undefined behavior to call any of the mutating functions while a pointer
   // to this surface is alive.
   UniqueCairoSurfacePtr GetCairoSurface();
+
+  int width() const { return width_; }
+  int height() const { return height_; }
 
  private:
   int width_;
--- cartographer/io/probability_grid_points_processor.cc
+++ cartographer/io/probability_grid_points_processor.cc
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 The Cartographer Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include "cartographer/io/probability_grid_points_processor.h"
 
 #include "Eigen/Core"
@@ -12,63 +28,28 @@ namespace cartographer {
 namespace io {
 namespace {
 
-void WriteGrid(
+void DrawTrajectoriesIntoImage(
     const mapping_2d::ProbabilityGrid& probability_grid,
-    const ProbabilityGridPointsProcessor::DrawTrajectories& draw_trajectories,
+    const Eigen::Array2i& offset,
     const std::vector<mapping::proto::Trajectory>& trajectories,
-    FileWriter* const file_writer) {
-  Eigen::Array2i offset;
-  mapping_2d::CellLimits cell_limits;
-  probability_grid.ComputeCroppedLimits(&offset, &cell_limits);
-  if (cell_limits.num_x_cells == 0 || cell_limits.num_y_cells == 0) {
-    LOG(WARNING) << "Not writing output: empty probability grid";
-    return;
+    cairo_surface_t* cairo_surface) {
+  for (size_t i = 0; i < trajectories.size(); ++i) {
+    DrawTrajectory(trajectories[i], GetColor(i),
+                   [&probability_grid,
+                    &offset](const transform::Rigid3d& pose) -> Eigen::Array2i {
+                     return probability_grid.limits().GetCellIndex(
+                                pose.cast<float>().translation().head<2>()) -
+                            offset;
+                   },
+                   cairo_surface);
   }
-  const auto compute_color_value = [&probability_grid](
-                                       const Eigen::Array2i& index) {
-    if (probability_grid.IsKnown(index)) {
-      const float probability = 1.f - probability_grid.GetProbability(index);
-      return static_cast<uint8>(
-          255 * ((probability - mapping::kMinProbability) /
-                 (mapping::kMaxProbability - mapping::kMinProbability)));
-    } else {
-      constexpr uint8 kUnknownValue = 128;
-      return kUnknownValue;
-    }
-  };
-  Image image(cell_limits.num_x_cells, cell_limits.num_y_cells);
-  for (const auto& xy_index :
-       cartographer::mapping_2d::XYIndexRangeIterator(cell_limits)) {
-    const auto index = xy_index + offset;
-    uint8 value = compute_color_value(index);
-    image.SetPixel(xy_index.x(), xy_index.y(), {{value, value, value}});
-  }
-
-  if (draw_trajectories ==
-      ProbabilityGridPointsProcessor::DrawTrajectories::kYes) {
-    for (size_t i = 0; i < trajectories.size(); ++i) {
-      DrawTrajectory(trajectories[i], GetColor(i),
-                     [&probability_grid, &offset](
-                         const transform::Rigid3d& pose) -> Eigen::Array2i {
-                       return probability_grid.limits().GetCellIndex(
-                                  pose.cast<float>().translation().head<2>()) -
-                              offset;
-                     },
-                     image.GetCairoSurface().get());
-    }
-  }
-  image.WritePng(file_writer);
-  CHECK(file_writer->Close());
 }
 
-mapping_2d::ProbabilityGrid CreateProbabilityGrid(const double resolution) {
-  constexpr int kInitialProbabilityGridSize = 100;
-  Eigen::Vector2d max =
-      0.5 * kInitialProbabilityGridSize * resolution * Eigen::Vector2d::Ones();
-  return mapping_2d::ProbabilityGrid(cartographer::mapping_2d::MapLimits(
-      resolution, max,
-      mapping_2d::CellLimits(kInitialProbabilityGridSize,
-                             kInitialProbabilityGridSize)));
+uint8 ProbabilityToColor(float probability_from_grid) {
+  const float probability = 1.f - probability_from_grid;
+  return ::cartographer::common::RoundToInt(
+      255 * ((probability - mapping::kMinProbability) /
+             (mapping::kMaxProbability - mapping::kMinProbability)));
 }
 
 }  // namespace
@@ -115,8 +96,19 @@ void ProbabilityGridPointsProcessor::Process(
 }
 
 PointsProcessor::FlushResult ProbabilityGridPointsProcessor::Flush() {
-  WriteGrid(probability_grid_, draw_trajectories_, trajectories_,
-            file_writer_.get());
+  Eigen::Array2i offset;
+  std::unique_ptr<Image> image =
+      DrawProbabilityGrid(probability_grid_, &offset);
+  if (image != nullptr) {
+    if (draw_trajectories_ ==
+        ProbabilityGridPointsProcessor::DrawTrajectories::kYes) {
+      DrawTrajectoriesIntoImage(probability_grid_, offset, trajectories_,
+                                image->GetCairoSurface().get());
+    }
+    image->WritePng(file_writer_.get());
+    CHECK(file_writer_->Close());
+  }
+
   switch (next_->Flush()) {
     case FlushResult::kRestartStream:
       LOG(FATAL) << "ProbabilityGrid generation must be configured to occur "
@@ -126,6 +118,41 @@ PointsProcessor::FlushResult ProbabilityGridPointsProcessor::Flush() {
       return FlushResult::kFinished;
   }
   LOG(FATAL);
+}
+
+std::unique_ptr<Image> DrawProbabilityGrid(
+    const mapping_2d::ProbabilityGrid& probability_grid,
+    Eigen::Array2i* offset) {
+  mapping_2d::CellLimits cell_limits;
+  probability_grid.ComputeCroppedLimits(offset, &cell_limits);
+  if (cell_limits.num_x_cells == 0 || cell_limits.num_y_cells == 0) {
+    LOG(WARNING) << "Not writing output: empty probability grid";
+    return nullptr;
+  }
+  auto image = common::make_unique<Image>(cell_limits.num_x_cells,
+                                          cell_limits.num_y_cells);
+  for (const auto& xy_index :
+       cartographer::mapping_2d::XYIndexRangeIterator(cell_limits)) {
+    const auto index = xy_index + *offset;
+    constexpr uint8 kUnknownValue = 128;
+    const uint8 value =
+        probability_grid.IsKnown(index)
+            ? ProbabilityToColor(probability_grid.GetProbability(index))
+            : kUnknownValue;
+    ;
+    image->SetPixel(xy_index.x(), xy_index.y(), {{value, value, value}});
+  }
+  return image;
+}
+
+mapping_2d::ProbabilityGrid CreateProbabilityGrid(const double resolution) {
+  constexpr int kInitialProbabilityGridSize = 100;
+  Eigen::Vector2d max =
+      0.5 * kInitialProbabilityGridSize * resolution * Eigen::Vector2d::Ones();
+  return mapping_2d::ProbabilityGrid(cartographer::mapping_2d::MapLimits(
+      resolution, max,
+      mapping_2d::CellLimits(kInitialProbabilityGridSize,
+                             kInitialProbabilityGridSize)));
 }
 
 }  // namespace io
