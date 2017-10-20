dda5146c759b15a6f97efd553c2b144f6b6edfe3
Juraj Oršulić <juraj.orsulic@fer.hr>
2017-10-20 14:56:23 +0200

    Reorder header

--- cartographer/mapping_2d/sparse_pose_graph/optimization_problem.h
+++ cartographer/mapping_2d/sparse_pose_graph/optimization_problem.h
@@ -65,14 +65,14 @@ class OptimizationProblem {
   void AddImuData(int trajectory_id, const sensor::ImuData& imu_data);
   void AddOdometerData(int trajectory_id,
                        const sensor::OdometryData& odometry_data);
-  void InsertTrajectoryNode(const mapping::NodeId& node_id, common::Time time,
-                            const transform::Rigid2d& initial_pose,
-                            const transform::Rigid2d& pose,
-                            const Eigen::Quaterniond& gravity_alignment);
   void AddTrajectoryNode(int trajectory_id, common::Time time,
                          const transform::Rigid2d& initial_pose,
                          const transform::Rigid2d& pose,
                          const Eigen::Quaterniond& gravity_alignment);
+  void InsertTrajectoryNode(const mapping::NodeId& node_id, common::Time time,
+                            const transform::Rigid2d& initial_pose,
+                            const transform::Rigid2d& pose,
+                            const Eigen::Quaterniond& gravity_alignment);
   void TrimTrajectoryNode(const mapping::NodeId& node_id);
   void AddSubmap(int trajectory_id, const transform::Rigid2d& submap_pose);
   void InsertSubmap(const mapping::SubmapId& submap_id,
