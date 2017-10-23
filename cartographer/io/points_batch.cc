a78dd772793eb252d171eea94d049488751ed8ad
Holger Rapp <hrapp@google.com>
2017-10-23 10:06:04 +0200

    Addressed reviewer comments.

--- cartographer/io/points_batch.cc
+++ cartographer/io/points_batch.cc
@@ -20,12 +20,17 @@ namespace cartographer {
 namespace io {
 
 void RemovePoints(std::unordered_set<int> to_remove, PointsBatch* batch) {
+  const int new_num_points = batch->points.size() - to_remove.size();
   std::vector<Eigen::Vector3f> points;
-  points.reserve(batch->points.size());
+  points.reserve(new_num_points);
   std::vector<float> intensities;
-  intensities.reserve(batch->intensities.size());
+  if (!batch->intensities.empty()) {
+    intensities.reserve(new_num_points);
+  }
   std::vector<FloatColor> colors;
-  colors.reserve(batch->colors.size());
+  if (!batch->colors.empty()) {
+    colors.reserve(new_num_points);
+  }
 
   for (size_t i = 0; i < batch->points.size(); ++i) {
     if (to_remove.count(i) == 1) {
