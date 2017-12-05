d9a9fdbe61024a848b0c39d38b4a61e0e30bfa35
Andre Gaschler <gaschler@google.com>
2017-12-05 15:32:53 +0100

    Test LocalSlamResultCallback

--- cartographer/mapping/map_builder_test.cc
+++ cartographer/mapping/map_builder_test.cc
@@ -25,6 +25,7 @@
 #include "cartographer/common/lua_parameter_dictionary_test_helpers.h"
 #include "cartographer/common/make_unique.h"
 #include "cartographer/mapping/trajectory_builder.h"
+#include "cartographer/transform/timestamped_transform.h"
 #include "gtest/gtest.h"
 
 namespace cartographer {
@@ -48,6 +49,7 @@ class MapBuilderTest : public ::testing::Test {
     const std::string kMapBuilderLua = R"text(
       include "map_builder.lua"
       MAP_BUILDER.use_trajectory_builder_2d = true
+      MAP_BUILDER.pose_graph.optimize_every_n_nodes = 1
       return MAP_BUILDER)text";
     auto parameter_dictionary = ResolveLuaParameters(kMapBuilderLua);
     proto::MapBuilderOptions options =
@@ -76,6 +78,57 @@ TEST_F(MapBuilderTest, TrajectoryAddFinish) {
   EXPECT_TRUE(map_builder_->GetTrajectoryBuilder(trajectory_id) != nullptr);
   EXPECT_TRUE(map_builder_->pose_graph() != nullptr);
   map_builder_->FinishTrajectory(trajectory_id);
+  EXPECT_TRUE(map_builder_->pose_graph()->IsTrajectoryFinished(trajectory_id));
+}
+
+TEST_F(MapBuilderTest, AddRangerfinder2D) {
+  const std::string kRangeSensorId = "lidar";
+  const std::string kTrajectoryBuilderLua = R"text(
+      include "trajectory_builder.lua"
+      TRAJECTORY_BUILDER.trajectory_builder_2d.use_imu_data = false
+      TRAJECTORY_BUILDER.trajectory_builder_2d.submaps.num_range_data = 5
+      return TRAJECTORY_BUILDER)text";
+  const std::unordered_set<std::string> expected_sensor_ids = {kRangeSensorId};
+  auto trajectory_builder_parameters =
+      ResolveLuaParameters(kTrajectoryBuilderLua);
+  proto::TrajectoryBuilderOptions trajectory_options =
+      CreateTrajectoryBuilderOptions(trajectory_builder_parameters.get());
+  MapBuilderInterface::LocalSlamResultCallback callback =
+      [](const int trajectory_id, const ::cartographer::common::Time time,
+         const ::cartographer::transform::Rigid3d local_pose,
+         ::cartographer::sensor::RangeData range_data_in_local,
+         const std::unique_ptr<const ::cartographer::mapping::NodeId>) {
+        LOG(WARNING) << "local_pose: " << local_pose;
+      };
+  int trajectory_id = map_builder_->AddTrajectoryBuilder(
+      expected_sensor_ids, trajectory_options, callback);
+  EXPECT_EQ(1, map_builder_->num_trajectory_builders());
+  EXPECT_TRUE(map_builder_->pose_graph() != nullptr);
+  TrajectoryBuilder* trajectory_builder =
+      map_builder_->GetTrajectoryBuilder(trajectory_id);
+  EXPECT_TRUE(trajectory_builder != nullptr);
+
+  const double kTimeStep = 0.1;
+  sensor::TimedPointCloud point_cloud;
+  for (double angle = 0.; angle < 1. * M_PI; angle += 0.01) {
+    const double kRadius = 3;
+    point_cloud.emplace_back(kRadius * std::cos(angle),
+                             kRadius * std::sin(angle), 0., 0.);
+  }
+  for (int step = 0; step < 10; ++step) {
+    double elapsed_time = step * kTimeStep;
+    common::Time time =
+        common::FromUniversal(12345678) + common::FromSeconds(elapsed_time);
+    transform::Rigid3f pose = transform::Rigid3f::Translation(
+        Eigen::Vector3f(elapsed_time * 0.5, elapsed_time * 0.05, 0.));
+    sensor::TimedPointCloud ranges =
+        sensor::TransformTimedPointCloud(point_cloud, pose.inverse());
+    trajectory_builder->AddRangefinderData(kRangeSensorId, time,
+                                           Eigen::Vector3f::Zero(), ranges);
+  }
+
+  map_builder_->FinishTrajectory(trajectory_id);
+  map_builder_->pose_graph()->RunFinalOptimization();
   EXPECT_TRUE(map_builder_->pose_graph()->IsTrajectoryFinished(trajectory_id));
 }
 
