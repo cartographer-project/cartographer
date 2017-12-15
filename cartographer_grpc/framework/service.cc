006b9d6054550a40607b537778b78d7e7e779949
Christoph Schütte <cschuet@google.com>
2017-12-15 13:52:21 +0100

    Refactor Write()/Finish() and make thread-safe.

--- cartographer_grpc/framework/service.cc
+++ cartographer_grpc/framework/service.cc
@@ -70,6 +70,9 @@ void Service::HandleEvent(Rpc::Event event, Rpc* rpc, bool ok) {
     case Rpc::Event::WRITE:
       HandleWrite(rpc, ok);
       break;
+    case Rpc::Event::WRITE_NEEDED:
+      HandleWriteNeeded(rpc, ok);
+      break;
     case Rpc::Event::FINISH:
       HandleFinish(rpc, ok);
       break;
@@ -114,6 +117,15 @@ void Service::HandleRead(Rpc* rpc, bool ok) {
 
   // Reads completed.
   rpc->OnReadsDone();
+
+  RemoveIfNotPending(rpc);
+}
+
+void Service::HandleWriteNeeded(Rpc* rpc, bool ok) {
+  CHECK(ok);
+
+  // Send the next message or potentially finish the connection.
+  rpc->PerformWriteIfNeeded();
 
   RemoveIfNotPending(rpc);
 }
