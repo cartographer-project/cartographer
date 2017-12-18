7dfa3db5310548a758502fd5dc8f2fcda44addef
Andre Gaschler <gaschler@google.com>
2017-12-18 10:31:12 +0100

    lazy writers

--- cartographer_grpc/mapping/trajectory_builder_stub.cc
+++ cartographer_grpc/mapping/trajectory_builder_stub.cc
@@ -27,30 +27,31 @@ TrajectoryBuilderStub::TrajectoryBuilderStub(
     : client_channel_(client_channel), trajectory_id_(trajectory_id) {
   stub_ = proto::MapBuilderService::NewStub(client_channel_);
   CHECK(stub_) << "Failed to create stub.";
-  rangefinder_writer_.client_writer = stub_->AddRangefinderData(
-      &rangefinder_writer_.client_context, &rangefinder_writer_.response);
-  CHECK(rangefinder_writer_.client_writer);
-  imu_writer_.client_writer =
-      stub_->AddImuData(&imu_writer_.client_context, &imu_writer_.response);
-  CHECK(imu_writer_.client_writer);
-  odometry_writer_.client_writer = stub_->AddOdometryData(
-      &odometry_writer_.client_context, &odometry_writer_.response);
-  CHECK(odometry_writer_.client_writer);
-  fixed_frame_writer_.client_writer = stub_->AddFixedFramePoseData(
-      &fixed_frame_writer_.client_context, &fixed_frame_writer_.response);
-  CHECK(fixed_frame_writer_.client_writer);
 }
 
 TrajectoryBuilderStub::~TrajectoryBuilderStub() {
-  CHECK(rangefinder_writer_.client_writer->Finish().ok());
-  CHECK(imu_writer_.client_writer->Finish().ok());
-  CHECK(odometry_writer_.client_writer->Finish().ok());
-  CHECK(fixed_frame_writer_.client_writer->Finish().ok());
+  if (rangefinder_writer_.client_writer) {
+    CHECK(rangefinder_writer_.client_writer->Finish().ok());
+  }
+  if (imu_writer_.client_writer) {
+    CHECK(imu_writer_.client_writer->Finish().ok());
+  }
+  if (odometry_writer_.client_writer) {
+    CHECK(odometry_writer_.client_writer->Finish().ok());
+  }
+  if (fixed_frame_writer_.client_writer) {
+    CHECK(fixed_frame_writer_.client_writer->Finish().ok());
+  }
 }
 
 void TrajectoryBuilderStub::AddSensorData(
     const std::string& sensor_id,
     const cartographer::sensor::TimedPointCloudData& timed_point_cloud_data) {
+  if (!rangefinder_writer_.client_writer) {
+    rangefinder_writer_.client_writer = stub_->AddRangefinderData(
+        &rangefinder_writer_.client_context, &rangefinder_writer_.response);
+    CHECK(rangefinder_writer_.client_writer);
+  }
   proto::AddRangefinderDataRequest request;
   *request.mutable_sensor_metadata() = CreateSensorMetadata(sensor_id);
   *request.mutable_timed_point_cloud_data() =
@@ -61,6 +62,11 @@ void TrajectoryBuilderStub::AddSensorData(
 void TrajectoryBuilderStub::AddSensorData(
     const std::string& sensor_id,
     const cartographer::sensor::ImuData& imu_data) {
+  if (!imu_writer_.client_writer) {
+    imu_writer_.client_writer =
+        stub_->AddImuData(&imu_writer_.client_context, &imu_writer_.response);
+    CHECK(imu_writer_.client_writer);
+  }
   proto::AddImuDataRequest request;
   *request.mutable_sensor_metadata() = CreateSensorMetadata(sensor_id);
   *request.mutable_imu_data() = cartographer::sensor::ToProto(imu_data);
@@ -70,6 +76,11 @@ void TrajectoryBuilderStub::AddSensorData(
 void TrajectoryBuilderStub::AddSensorData(
     const std::string& sensor_id,
     const cartographer::sensor::OdometryData& odometry_data) {
+  if (!odometry_writer_.client_writer) {
+    odometry_writer_.client_writer = stub_->AddOdometryData(
+        &odometry_writer_.client_context, &odometry_writer_.response);
+    CHECK(odometry_writer_.client_writer);
+  }
   proto::AddOdometryDataRequest request;
   *request.mutable_sensor_metadata() = CreateSensorMetadata(sensor_id);
   *request.mutable_odometry_data() =
@@ -80,6 +91,11 @@ void TrajectoryBuilderStub::AddSensorData(
 void TrajectoryBuilderStub::AddSensorData(
     const std::string& sensor_id,
     const cartographer::sensor::FixedFramePoseData& fixed_frame_pose) {
+  if (!fixed_frame_writer_.client_writer) {
+    fixed_frame_writer_.client_writer = stub_->AddFixedFramePoseData(
+        &fixed_frame_writer_.client_context, &fixed_frame_writer_.response);
+    CHECK(fixed_frame_writer_.client_writer);
+  }
   proto::AddFixedFramePoseDataRequest request;
   *request.mutable_sensor_metadata() = CreateSensorMetadata(sensor_id);
   *request.mutable_fixed_frame_pose_data() =
