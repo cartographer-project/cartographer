0440761474a6deea2ae4e908b340fc0878e7a915
Alexander Belyaev <32522095+pifon2a@users.noreply.github.com>
2018-01-31 17:06:49 +0100

    Move slerp and scaling of error to 'cost_helpers'. (#864)
    [RFC=0011](https://github.com/googlecartographer/rfcs/blob/master/text/0011-landmarks.md)

--- cartographer/mapping/pose_graph/cost_helpers_impl.h
+++ cartographer/mapping/pose_graph/cost_helpers_impl.h
@@ -22,7 +22,7 @@ namespace mapping {
 namespace pose_graph {
 
 template <typename T>
-static std::array<T, 3> ComputeUnscaledError2d(
+static std::array<T, 3> ComputeUnscaledError(
     const transform::Rigid2d& relative_pose, const T* const start,
     const T* const end) {
   const T cos_theta_i = cos(start[2]);
@@ -39,7 +39,17 @@ static std::array<T, 3> ComputeUnscaledError2d(
 }
 
 template <typename T>
-static std::array<T, 6> ComputeUnscaledError3d(
+std::array<T, 3> ScaleError(std::array<T, 3> error, T translation_weight,
+                            T rotation_weight) {
+  std::array<T, 3> scaled_error(std::move(error));
+  scaled_error[0] *= translation_weight;
+  scaled_error[1] *= translation_weight;
+  scaled_error[2] *= rotation_weight;
+  return scaled_error;
+}
+
+template <typename T>
+static std::array<T, 6> ComputeUnscaledError(
     const transform::Rigid3d& relative_pose, const T* const start_rotation,
     const T* const start_translation, const T* const end_rotation,
     const T* const end_translation) {
@@ -67,6 +77,46 @@ static std::array<T, 6> ComputeUnscaledError3d(
            T(relative_pose.translation().z()) - h_translation[2],
            angle_axis_difference[0], angle_axis_difference[1],
            angle_axis_difference[2]}};
+}
+
+template <typename T>
+std::array<T, 6> ScaleError(std::array<T, 6> error, T translation_weight,
+                            T rotation_weight) {
+  std::array<T, 6> scaled_error(std::move(error));
+  scaled_error[0] *= translation_weight;
+  scaled_error[1] *= translation_weight;
+  scaled_error[2] *= translation_weight;
+  scaled_error[3] *= rotation_weight;
+  scaled_error[4] *= rotation_weight;
+  scaled_error[5] *= rotation_weight;
+  return scaled_error;
+}
+
+//  Eigen implementation of slerp is not compatible with Ceres on all supported
+//  platforms. Our own implementation is used instead.
+template <typename T>
+std::array<T, 4> SlerpQuaternions(const T* const start, const T* const end,
+                                  T factor) {
+  // Angle 'theta' is the half-angle "between" quaternions. It can be computed
+  // as the arccosine of their dot product.
+  const T cos_theta = start[0] * end[0] + start[1] * end[1] +
+                      start[2] * end[2] + start[3] * end[3];
+  const T abs_cos_theta = abs(cos_theta);
+  // If numerical error brings 'cos_theta' outside [-1 + epsilon, 1 - epsilon]
+  // interval, then the quaternions are likely to be collinear.
+  T prev_scale = T(1.) - factor;
+  T next_scale = factor;
+  if (abs_cos_theta < T(1. - 1e-5)) {
+    const T theta = acos(abs_cos_theta);
+    const T sin_theta = sin(theta);
+    prev_scale = sin(prev_scale * theta) / sin_theta;
+    next_scale = sin(next_scale * theta) / sin_theta;
+  }
+  if (cos_theta < T(0.)) next_scale = -next_scale;
+  return {{prev_scale * start[0] + next_scale * end[0],
+           prev_scale * start[1] + next_scale * end[1],
+           prev_scale * start[2] + next_scale * end[2],
+           prev_scale * start[3] + next_scale * end[3]}};
 }
 
 }  // namespace pose_graph
