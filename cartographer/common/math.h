8781a4a7eb065c55a95dfce216dfd9e1e7526fb7
Juraj Oršulić <juraj.orsulic@fer.hr>
2018-03-16 15:37:22 +0100

    Fixup.

--- cartographer/common/math.h
+++ cartographer/common/math.h
@@ -75,8 +75,8 @@ T atan2(const Eigen::Matrix<T, 2, 1>& vector) {
 }
 
 template <typename T>
-inline void QuaternionProduct(const double* const z, const T* const w[4],
-                              T* const zw[4]) {
+inline void QuaternionProduct(const double* const z, const T* const w,
+                              T* const zw) {
   zw[0] = z[0] * w[0] - z[1] * w[1] - z[2] * w[2] - z[3] * w[3];
   zw[1] = z[0] * w[1] + z[1] * w[0] + z[2] * w[3] - z[3] * w[2];
   zw[2] = z[0] * w[2] - z[1] * w[3] + z[2] * w[0] + z[3] * w[1];
--- cartographer/mapping/internal/3d/scan_matching/rotation_delta_cost_functor_3d.h
+++ cartographer/mapping/internal/3d/scan_matching/rotation_delta_cost_functor_3d.h
@@ -44,7 +44,7 @@ class RotationDeltaCostFunctor3D {
   bool operator()(const T* const rotation_quaternion, T* residual) const {
     std::array<T, 4> delta;
     common::QuaternionProduct(target_rotation_inverse_, rotation_quaternion,
-                              delta);
+                              delta.data());
     // Will compute the squared norm of the imaginary component of the delta
     // quaternion which is sin(phi/2)^2.
     residual[0] = scaling_factor_ * delta[1];
