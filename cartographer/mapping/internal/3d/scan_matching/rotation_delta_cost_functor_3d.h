6853d288aecd50d02602cbda613df8b9f643363d
Juraj Oršulić <juraj.orsulic@fer.hr>
2018-03-16 15:14:55 +0100

    Address review comments.

--- cartographer/mapping/internal/3d/scan_matching/rotation_delta_cost_functor_3d.h
+++ cartographer/mapping/internal/3d/scan_matching/rotation_delta_cost_functor_3d.h
@@ -42,9 +42,9 @@ class RotationDeltaCostFunctor3D {
 
   template <typename T>
   bool operator()(const T* const rotation_quaternion, T* residual) const {
-    T delta[4];
-    cartographer::common::QuaternionProduct(target_rotation_inverse_,
-                                            rotation_quaternion, delta);
+    std::array<T,4> delta;
+    common::QuaternionProduct(target_rotation_inverse_, rotation_quaternion,
+                              delta);
     // Will compute the squared norm of the imaginary component of the delta
     // quaternion which is sin(phi/2)^2.
     residual[0] = scaling_factor_ * delta[1];
