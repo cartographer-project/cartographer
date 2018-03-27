962393074a4418963696c9aea771db35a6a991a5
Alexander Belyaev <32522095+pifon2a@users.noreply.github.com>
2018-03-27 14:26:21 +0200

    New fixes for Jets. (#1023)
    Issue #1015

--- cartographer/common/math.h
+++ cartographer/common/math.h
@@ -60,12 +60,9 @@ constexpr double RadToDeg(double rad) { return 180. * rad / M_PI; }
 // Bring the 'difference' between two angles into [-pi; pi].
 template <typename T>
 T NormalizeAngleDifference(T difference) {
-  while (difference > M_PI) {
-    difference -= 2. * M_PI;
-  }
-  while (difference < -M_PI) {
-    difference += 2. * M_PI;
-  }
+  const T kPi = T(M_PI);
+  while (difference > kPi) difference -= 2. * kPi;
+  while (difference < -kPi) difference += 2. * kPi;
   return difference;
 }
 
