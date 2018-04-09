e89625d36aebaf427d9d633c117d7a5d20e2ef14
gaschler <gaschlera@gmail.com>
2018-04-09 14:29:45 +0200

    Correctly use PrecomputationGridStack for 2D and 3D (#1040)
    FIXES=#1038

--- cartographer/mapping/internal/2d/scan_matching/fast_correlative_scan_matcher_2d.h
+++ cartographer/mapping/internal/2d/scan_matching/fast_correlative_scan_matcher_2d.h
@@ -92,7 +92,21 @@ class PrecomputationGrid2D {
   std::vector<uint8> cells_;
 };
 
-class PrecomputationGridStack;
+class PrecomputationGridStack2D {
+ public:
+  PrecomputationGridStack2D(
+      const ProbabilityGrid& probability_grid,
+      const proto::FastCorrelativeScanMatcherOptions2D& options);
+
+  const PrecomputationGrid2D& Get(int index) {
+    return precomputation_grids_[index];
+  }
+
+  int max_depth() const { return precomputation_grids_.size() - 1; }
+
+ private:
+  std::vector<PrecomputationGrid2D> precomputation_grids_;
+};
 
 // An implementation of "Real-Time Correlative Scan Matching" by Olson.
 class FastCorrelativeScanMatcher2D {
@@ -146,7 +160,7 @@ class FastCorrelativeScanMatcher2D {
 
   const proto::FastCorrelativeScanMatcherOptions2D options_;
   MapLimits limits_;
-  std::unique_ptr<PrecomputationGridStack> precomputation_grid_stack_;
+  std::unique_ptr<PrecomputationGridStack2D> precomputation_grid_stack_;
 };
 
 }  // namespace scan_matching
