5356bdf9a83bbb1255f0f56bc476a61619fb8e8b
Christoph Schütte <cschuet@google.com>
2018-04-25 17:34:30 +0200

    Rename  metrics since slashes are not allowed in Prometheus (#1115)

--- cartographer/mapping/internal/3d/local_trajectory_builder_3d.cc
+++ cartographer/mapping/internal/3d/local_trajectory_builder_3d.cc
@@ -300,24 +300,24 @@ LocalTrajectoryBuilder3D::InsertIntoSubmap(
 void LocalTrajectoryBuilder3D::RegisterMetrics(
     metrics::FamilyFactory* family_factory) {
   auto* latency = family_factory->NewGaugeFamily(
-      "/mapping/internal/3d/local_trajectory_builder/latency",
+      "mapping_internal_3d_local_trajectory_builder_latency",
       "Duration from first incoming point cloud in accumulation to local slam "
       "result");
   kLocalSlamLatencyMetric = latency->Add({});
   auto score_boundaries = metrics::Histogram::FixedWidth(0.05, 20);
   auto* scores = family_factory->NewHistogramFamily(
-      "/mapping/internal/3d/local_trajectory_builder/scores",
+      "mapping_internal_3d_local_trajectory_builder_scores",
       "Local scan matcher scores", score_boundaries);
   kRealTimeCorrelativeScanMatcherScoreMetric =
       scores->Add({{"scan_matcher", "real_time_correlative"}});
   auto cost_boundaries = metrics::Histogram::ScaledPowersOf(2, 0.01, 100);
   auto* costs = family_factory->NewHistogramFamily(
-      "/mapping/internal/3d/local_trajectory_builder/costs",
+      "mapping_internal_3d_local_trajectory_builder_costs",
       "Local scan matcher costs", cost_boundaries);
   kCeresScanMatcherCostMetric = costs->Add({{"scan_matcher", "ceres"}});
   auto distance_boundaries = metrics::Histogram::ScaledPowersOf(2, 0.01, 10);
   auto* residuals = family_factory->NewHistogramFamily(
-      "/mapping/internal/3d/local_trajectory_builder/residuals",
+      "mapping_internal_3d_local_trajectory_builder_residuals",
       "Local scan matcher residuals", distance_boundaries);
   kScanMatcherResidualDistanceMetric =
       residuals->Add({{"component", "distance"}});
