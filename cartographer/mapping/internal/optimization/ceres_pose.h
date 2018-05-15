a0ec8046bddcf2f56080e6157d10337e5a152a45
Robert Milijaš <robert.milijas29@gmail.com>
2018-05-15 22:32:03 +0200

    Allow easier access to CeresPose's data_ (#1149)
    Introduce a FromPose function which transforms a Ridig3d pose into the
    CeresPose's Data format.

--- cartographer/mapping/internal/optimization/ceres_pose.h
+++ cartographer/mapping/internal/optimization/ceres_pose.h
@@ -44,14 +44,19 @@ class CeresPose {
   double* rotation() { return data_->rotation.data(); }
   const double* rotation() const { return data_->rotation.data(); }
 
- private:
   struct Data {
     std::array<double, 3> translation;
     // Rotation quaternion as (w, x, y, z).
     std::array<double, 4> rotation;
   };
+
+  Data& data() { return *data_; }
+
+ private:
   std::shared_ptr<Data> data_;
 };
+
+CeresPose::Data FromPose(const transform::Rigid3d& pose);
 
 }  // namespace optimization
 }  // namespace mapping
