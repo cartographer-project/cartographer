b4594bcdbdfd84e3c20072b55a41b8d8fac3a030
Kevin Daun <kdaun@google.com>
2018-06-26 14:22:16 +0200

    Add Normal Estimation (#1213)
    Adds Normal Estimation needed for TSDF RangeDataInserter.

---
+++ cartographer/mapping/internal/2d/normal_estimation_2d.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2018 The Cartographer Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef CARTOGRAPHER_MAPPING_INTERNAL_NORMAL_ESTIMATION_2D_H_
+#define CARTOGRAPHER_MAPPING_INTERNAL_NORMAL_ESTIMATION_2D_H_
+
+#include <vector>
+#include "cartographer/mapping/proto/2d/normal_estimation_options_2d.pb.h"
+#include "cartographer/sensor/point_cloud.h"
+#include "cartographer/sensor/range_data.h"
+#include "cartographer/transform/transform.h"
+
+namespace cartographer {
+namespace mapping {
+
+proto::NormalEstimationOptions2D CreateNormalEstimationOptions2D(
+    common::LuaParameterDictionary* parameter_dictionary);
+
+// Estimates the normal for each 'return' in 'range_data'.
+// Assumes the angles in the range data returns are sorted with respect to
+// the orientation of the vector from 'origin' to 'return'.
+std::vector<float> EstimateNormals(
+    const sensor::RangeData& range_data,
+    const proto::NormalEstimationOptions2D& normal_estimation_options);
+
+}  // namespace mapping
+}  // namespace cartographer
+
+#endif  // CARTOGRAPHER_MAPPING_INTERNAL_NORMAL_ESTIMATION_2D_H_
---
+++ cartographer/mapping/internal/2d/normal_estimation_2d_test.cc
@@ -0,0 +1,144 @@
+/*
+ * Copyright 2018 The Cartographer Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "cartographer/mapping/internal/2d/normal_estimation_2d.h"
+
+#include "cartographer/common/lua_parameter_dictionary.h"
+#include "cartographer/common/lua_parameter_dictionary_test_helpers.h"
+#include "cartographer/common/math.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+namespace cartographer {
+namespace mapping {
+namespace {
+
+using ::testing::TestWithParam;
+using ::testing::Values;
+
+TEST(NormalEstimation2DTest, SinglePoint) {
+  const auto parameter_dictionary = common::MakeDictionary(
+      "return { "
+      "num_normal_samples = 2, "
+      "sample_radius = 10.0, "
+      "}");
+  const proto::NormalEstimationOptions2D options =
+      CreateNormalEstimationOptions2D(parameter_dictionary.get());
+  sensor::RangeData range_data;
+  const size_t num_angles = 100;
+  range_data.origin.x() = 0.f;
+  range_data.origin.y() = 0.f;
+  for (size_t angle_idx = 0; angle_idx < num_angles; ++angle_idx) {
+    const double angle = static_cast<double>(angle_idx) /
+                             static_cast<double>(num_angles) * 2. * M_PI -
+                         M_PI;
+    range_data.returns.clear();
+    range_data.returns.emplace_back(std::cos(angle), std::sin(angle), 0.f);
+    std::vector<float> normals;
+    normals = EstimateNormals(range_data, options);
+    EXPECT_NEAR(common::NormalizeAngleDifference(angle - normals[0] - M_PI),
+                0.0, 2.0 * M_PI / num_angles + 1e-4);
+  }
+}
+
+TEST(NormalEstimation2DTest, StraightLineGeometry) {
+  const auto parameter_dictionary = common::MakeDictionary(
+      "return { "
+      "num_normal_samples = 2, "
+      "sample_radius = 10.0, "
+      "}");
+  const proto::NormalEstimationOptions2D options =
+      CreateNormalEstimationOptions2D(parameter_dictionary.get());
+  sensor::RangeData range_data;
+  range_data.returns.emplace_back(-1.f, 1.f, 0.f);
+  range_data.returns.emplace_back(0.f, 1.f, 0.f);
+  range_data.returns.emplace_back(1.f, 1.f, 0.f);
+  range_data.origin.x() = 0.f;
+  range_data.origin.y() = 0.f;
+  std::vector<float> normals;
+  normals = EstimateNormals(range_data, options);
+  for (const float normal : normals) {
+    EXPECT_NEAR(normal, -M_PI_2, 1e-4);
+  }
+  normals.clear();
+  range_data.returns.clear();
+  range_data.returns.emplace_back(1.f, 1.f, 0.f);
+  range_data.returns.emplace_back(1.f, 0.f, 0.f);
+  range_data.returns.emplace_back(1.f, -1.f, 0.f);
+  normals = EstimateNormals(range_data, options);
+  for (const float normal : normals) {
+    EXPECT_NEAR(std::abs(normal), M_PI, 1e-4);
+  }
+
+  normals.clear();
+  range_data.returns.clear();
+  range_data.returns.emplace_back(1.f, -1.f, 0.f);
+  range_data.returns.emplace_back(0.f, -1.f, 0.f);
+  range_data.returns.emplace_back(-1.f, -1.f, 0.f);
+  normals = EstimateNormals(range_data, options);
+  for (const float normal : normals) {
+    EXPECT_NEAR(normal, M_PI_2, 1e-4);
+  }
+
+  normals.clear();
+  range_data.returns.clear();
+  range_data.returns.emplace_back(-1.f, -1.f, 0.f);
+  range_data.returns.emplace_back(-1.f, 0.f, 0.f);
+  range_data.returns.emplace_back(-1.f, 1.f, 0.f);
+  normals = EstimateNormals(range_data, options);
+  for (const float normal : normals) {
+    EXPECT_NEAR(normal, 0, 1e-4);
+  }
+}
+
+class CircularGeometry2DTest : public TestWithParam<int> {};
+
+TEST_P(CircularGeometry2DTest, NumSamplesPerNormal) {
+  const auto parameter_dictionary = common::MakeDictionary(
+      "return { "
+      "num_normal_samples = " +
+      std::to_string(GetParam()) +
+      ", "
+      "sample_radius = 10.0, "
+      "}");
+  const proto::NormalEstimationOptions2D options =
+      CreateNormalEstimationOptions2D(parameter_dictionary.get());
+  sensor::RangeData range_data;
+  const size_t num_angles = 100;
+  for (size_t angle_idx = 0; angle_idx < num_angles; ++angle_idx) {
+    const double angle = static_cast<double>(angle_idx) /
+                             static_cast<double>(num_angles) * 2. * M_PI -
+                         M_PI;
+    range_data.returns.emplace_back(std::cos(angle), std::sin(angle), 0.f);
+  }
+  range_data.origin.x() = 0.f;
+  range_data.origin.y() = 0.f;
+  std::vector<float> normals;
+  normals = EstimateNormals(range_data, options);
+  for (size_t angle_idx = 0; angle_idx < num_angles; ++angle_idx) {
+    const double angle = static_cast<double>(angle_idx) /
+                         static_cast<double>(num_angles) * 2. * M_PI;
+    EXPECT_NEAR(common::NormalizeAngleDifference(normals[angle_idx] - angle),
+                0.0, 2.0 * M_PI / num_angles * GetParam() / 2.0 + 1e-4);
+  }
+}
+
+INSTANTIATE_TEST_CASE_P(InstantiationName, CircularGeometry2DTest,
+                        ::testing::Values(1, 2, 4, 5, 8));
+
+}  // namespace
+}  // namespace mapping
+}  // namespace cartographer
