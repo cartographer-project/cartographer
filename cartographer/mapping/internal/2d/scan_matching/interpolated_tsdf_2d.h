6c1c2577a2709d8cffe64fed83342ce345a7182d
Kevin Daun <kdaun@google.com>
2018-07-13 11:19:43 +0200

    Adress comments.

--- cartographer/mapping/internal/2d/scan_matching/interpolated_tsdf_2d.h
+++ cartographer/mapping/internal/2d/scan_matching/interpolated_tsdf_2d.h
@@ -46,20 +46,20 @@ class InterpolatedTSDF2D {
     const Eigen::Array2i index1 =
         tsdf_.limits().GetCellIndex(Eigen::Vector2f(x1, y1));
 
-    const double w11 = tsdf_.GetWeight(index1);
-    const double w12 = tsdf_.GetWeight(index1 + Eigen::Array2i(-1, 0));
-    const double w21 = tsdf_.GetWeight(index1 + Eigen::Array2i(0, -1));
-    const double w22 = tsdf_.GetWeight(index1 + Eigen::Array2i(-1, -1));
+    const float w11 = tsdf_.GetWeight(index1);
+    const float w12 = tsdf_.GetWeight(index1 + Eigen::Array2i(-1, 0));
+    const float w21 = tsdf_.GetWeight(index1 + Eigen::Array2i(0, -1));
+    const float w22 = tsdf_.GetWeight(index1 + Eigen::Array2i(-1, -1));
     if (w11 == 0.0 || w12 == 0.0 || w21 == 0.0 || w22 == 0.0) {
       return T(tsdf_.GetMaxCorrespondenceCost());
     }
 
-    const double q11 = tsdf_.GetCorrespondenceCost(index1);
-    const double q12 =
+    const float q11 = tsdf_.GetCorrespondenceCost(index1);
+    const float q12 =
         tsdf_.GetCorrespondenceCost(index1 + Eigen::Array2i(-1, 0));
-    const double q21 =
+    const float q21 =
         tsdf_.GetCorrespondenceCost(index1 + Eigen::Array2i(0, -1));
-    const double q22 =
+    const float q22 =
         tsdf_.GetCorrespondenceCost(index1 + Eigen::Array2i(-1, -1));
 
     return InterpolateBilinear(x, y, x1, y1, x2, y2, q11, q12, q21, q22);
@@ -73,10 +73,10 @@ class InterpolatedTSDF2D {
 
     const Eigen::Array2i index1 =
         tsdf_.limits().GetCellIndex(Eigen::Vector2f(x1, y1));
-    const double q11 = tsdf_.GetWeight(index1);
-    const double q12 = tsdf_.GetWeight(index1 + Eigen::Array2i(-1, 0));
-    const double q21 = tsdf_.GetWeight(index1 + Eigen::Array2i(0, -1));
-    const double q22 = tsdf_.GetWeight(index1 + Eigen::Array2i(-1, -1));
+    const float q11 = tsdf_.GetWeight(index1);
+    const float q12 = tsdf_.GetWeight(index1 + Eigen::Array2i(-1, 0));
+    const float q21 = tsdf_.GetWeight(index1 + Eigen::Array2i(0, -1));
+    const float q22 = tsdf_.GetWeight(index1 + Eigen::Array2i(-1, -1));
 
     return InterpolateBilinear(x, y, x1, y1, x2, y2, q11, q12, q21, q22);
   }
@@ -86,7 +86,7 @@ class InterpolatedTSDF2D {
   void ComputeInterpolationDataPoints(const T& x, const T& y, double* x1,
                                       double* y1, double* x2,
                                       double* y2) const {
-    const Eigen::Vector2f lower = CenterOfLowerVoxel(x, y);
+    const Eigen::Vector2f lower = CenterOfLowerPixel(x, y);
     *x1 = lower.x();
     *y1 = lower.y();
     *x2 = lower.x() + tsdf_.limits().resolution();
@@ -95,8 +95,8 @@ class InterpolatedTSDF2D {
 
   template <typename T>
   T InterpolateBilinear(const T& x, const T& y, double x1, double y1, double x2,
-                        double y2, double q11, double q12, double q21,
-                        double q22) const {
+                        double y2, float q11, float q12, float q21,
+                        float q22) const {
     const T normalized_x = (x - x1) / (x2 - x1);
     const T normalized_y = (y - y1) / (y2 - y1);
     const T q1 = (q12 - q11) * normalized_y + q11;
@@ -104,14 +104,14 @@ class InterpolatedTSDF2D {
     return (q2 - q1) * normalized_x + q1;
   }
 
-  // Center of the next lower voxel, i.e., not necessarily the voxel containing
-  // (x, y). For each dimension, the largest voxel index so that the
+  // Center of the next lower pixel, i.e., not necessarily the pixel containing
+  // (x, y). For each dimension, the largest pixel index so that the
   // corresponding center is at most the given coordinate.
-  Eigen::Vector2f CenterOfLowerVoxel(const double x, const double y) const {
+  Eigen::Vector2f CenterOfLowerPixel(const double x, const double y) const {
     // Center of the cell containing (x, y).
     Eigen::Vector2f center = tsdf_.limits().GetCellCenter(
         tsdf_.limits().GetCellIndex(Eigen::Vector2f(x, y)));
-    // Move to the next lower voxel center.
+    // Move to the next lower pixel center.
     if (center.x() > x) {
       center.x() -= tsdf_.limits().resolution();
     }
@@ -123,8 +123,8 @@ class InterpolatedTSDF2D {
 
   // Uses the scalar part of a Ceres Jet.
   template <typename T>
-  Eigen::Vector2f CenterOfLowerVoxel(const T& jet_x, const T& jet_y) const {
-    return CenterOfLowerVoxel(jet_x.a, jet_y.a);
+  Eigen::Vector2f CenterOfLowerPixel(const T& jet_x, const T& jet_y) const {
+    return CenterOfLowerPixel(jet_x.a, jet_y.a);
   }
 
   const TSDF2D& tsdf_;
