c3ad50cdf526d5464de7acb4b83b9eb2e41b24c1
Daniel Sievers <dsievers@lyft.com>
2018-07-16 10:58:33 +0200

    Introduce separate mutex for PoseGraph3D work queue access
    This reduces lock contention, see issue #1250.

--- cartographer/mapping/internal/3d/pose_graph_3d.cc
+++ cartographer/mapping/internal/3d/pose_graph_3d.cc
@@ -51,7 +51,7 @@ PoseGraph3D::PoseGraph3D(
 
 PoseGraph3D::~PoseGraph3D() {
   WaitForAllComputations();
-  common::MutexLocker locker(&mutex_);
+  common::MutexLocker locker(&work_queue_mutex_);
   CHECK(work_queue_ == nullptr);
 }
 
@@ -153,7 +153,7 @@ NodeId PoseGraph3D::AddNode(
 void PoseGraph3D::AddWorkItem(
     const std::function<WorkItem::Result()>& work_item) {
   {
-    common::MutexLocker locker(&mutex_);
+    common::MutexLocker locker(&work_queue_mutex_);
     if (work_queue_ != nullptr) {
       const auto now = std::chrono::steady_clock::now();
       work_queue_->push_back({now, work_item});
@@ -164,7 +164,7 @@ void PoseGraph3D::AddWorkItem(
   }
   if (work_item() == WorkItem::Result::kRunOptimization) {
     {
-      common::MutexLocker locker(&mutex_);
+      common::MutexLocker locker(&work_queue_mutex_);
       work_queue_ = common::make_unique<WorkQueue>();
     }
     constraint_builder_.WhenDone(
@@ -489,7 +489,7 @@ void PoseGraph3D::HandleWorkQueue(
   while (process_work_queue) {
     std::function<WorkItem::Result()> work_item;
     {
-      common::MutexLocker locker(&mutex_);
+      common::MutexLocker locker(&work_queue_mutex_);
       if (work_queue_->empty()) {
         work_queue_.reset();
         return;
@@ -509,40 +509,58 @@ void PoseGraph3D::HandleWorkQueue(
 }
 
 void PoseGraph3D::WaitForAllComputations() {
-  bool notification = false;
-  common::MutexLocker locker(&mutex_);
+  int num_trajectory_nodes;
+  {
+    common::MutexLocker locker(&mutex_);
+    num_trajectory_nodes = data_.num_trajectory_nodes;
+  }
+
   const int num_finished_nodes_at_start =
       constraint_builder_.GetNumFinishedNodes();
+
+  // First wait for the work queue to drain so that it's safe to schedule
+  // a WhenDone() callback.
+  {
+    common::MutexLocker locker(&work_queue_mutex_);
+    locker.Await(
+        [this]() REQUIRES(work_queue_mutex_) { return work_queue_ == nullptr; });
+  }
+
+  // Now wait for any pending constraint computations to finish.
+  common::Mutex notification_mutex;
+  common::MutexLocker locker(&notification_mutex);
+  bool notification GUARDED_BY(notification_mutex) = false;
+  constraint_builder_.WhenDone(
+      [this, &notification, &notification_mutex](
+          const constraints::ConstraintBuilder3D::Result& result)
+          EXCLUDES(mutex_) EXCLUDES(notification_mutex) {
+            {
+              common::MutexLocker locker(&mutex_);
+              data_.constraints.insert(data_.constraints.end(), result.begin(),
+                                       result.end());
+            }
+            {
+              common::MutexLocker locker(&notification_mutex);
+              notification = true;
+            }
+          });
   while (!locker.AwaitWithTimeout(
-      [this]() REQUIRES(mutex_) {
-        return ((constraint_builder_.GetNumFinishedNodes() ==
-                 data_.num_trajectory_nodes) &&
-                !work_queue_);
-      },
+      [&notification]() REQUIRES(notification_mutex) { return notification; },
       common::FromSeconds(1.))) {
     // Log progress on nodes only when we are actually processing nodes.
-    if (data_.num_trajectory_nodes != num_finished_nodes_at_start) {
+    if (num_trajectory_nodes != num_finished_nodes_at_start) {
       std::ostringstream progress_info;
       progress_info << "Optimizing: " << std::fixed << std::setprecision(1)
                     << 100. *
-                           (constraint_builder_.GetNumFinishedNodes() -
-                            num_finished_nodes_at_start) /
-                           (data_.num_trajectory_nodes -
-                            num_finished_nodes_at_start)
-                    << "%...";
+                         (constraint_builder_.GetNumFinishedNodes() -
+                          num_finished_nodes_at_start) /
+                         (num_trajectory_nodes - num_finished_nodes_at_start)
+                  << "%...";
       std::cout << "\r\x1b[K" << progress_info.str() << std::flush;
     }
   }
+  CHECK_EQ(constraint_builder_.GetNumFinishedNodes(), num_trajectory_nodes);
   std::cout << "\r\x1b[KOptimizing: Done.     " << std::endl;
-  constraint_builder_.WhenDone(
-      [this,
-       &notification](const constraints::ConstraintBuilder3D::Result& result) {
-        common::MutexLocker locker(&mutex_);
-        data_.constraints.insert(data_.constraints.end(), result.begin(),
-                                 result.end());
-        notification = true;
-      });
-  locker.Await([&notification]() { return notification; });
 }
 
 void PoseGraph3D::DeleteTrajectory(const int trajectory_id) {
