09046c9b7a834a34608357a8a0d47d6c05f7b609
Kevin Daun <kdaun@google.com>
2018-08-01 11:40:26 +0200

    Add TSDF match cost function.

---
+++ cartographer/mapping/internal/2d/scan_matching/tsdf_match_cost_function_2d.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2018 The Cartographer Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef CARTOGRAPHER_MAPPING_INTERNAL_2D_SCAN_MATCHING_TSDF_MATCH_COST_FUNCTION_2D_H_
+#define CARTOGRAPHER_MAPPING_INTERNAL_2D_SCAN_MATCHING_TSDF_MATCH_COST_FUNCTION_2D_H_
+
+#include "cartographer/mapping/2d/tsdf_2d.h"
+#include "cartographer/sensor/point_cloud.h"
+#include "ceres/ceres.h"
+
+namespace cartographer {
+namespace mapping {
+namespace scan_matching {
+
+// Creates a cost function for matching the 'point_cloud' in the 'grid' at
+// a 'pose'. The cost increases with the signed distance of the matched point
+// location in the 'grid'.
+ceres::CostFunction* CreateTSDFMatchCostFunction2D(
+    const double scaling_factor, const sensor::PointCloud& point_cloud,
+    const TSDF2D& grid);
+
+}  // namespace scan_matching
+}  // namespace mapping
+}  // namespace cartographer
+
+#endif  // CARTOGRAPHER_MAPPING_INTERNAL_2D_SCAN_MATCHING_TSDF_MATCH_COST_FUNCTION_2D_H_
