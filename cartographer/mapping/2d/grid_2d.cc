adaef7086471a643fb1699ac6a2c66142de194a2
Juraj Oršulić <juraj.orsulic@fer.hr>
2018-09-11 16:35:51 +0200

    Update grid_2d.cc

--- cartographer/mapping/2d/grid_2d.cc
+++ cartographer/mapping/2d/grid_2d.cc
@@ -106,6 +106,20 @@ void Grid2D::FinishUpdate() {
     update_indices_.pop_back();
   }
 }
+  
+// Fills in 'offset' and 'limits' to define a subregion of that contains all
+// known cells.
+void Grid2D::ComputeCroppedLimits(Eigen::Array2i* const offset,
+                                  CellLimits* const limits) const {
+  if (known_cells_box_.isEmpty()) {
+    *offset = Eigen::Array2i::Zero();
+    *limits = CellLimits(1, 1);
+    return;
+  }
+  *offset = known_cells_box_.min().array();
+  *limits = CellLimits(known_cells_box_.sizes().x() + 1,
+                       known_cells_box_.sizes().y() + 1);
+}
 
 void Grid2D::GrowLimits(const Eigen::Vector2f& point,
                         const std::vector<std::vector<uint16>*>& grids,
