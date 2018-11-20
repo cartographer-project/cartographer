14fa652279ec545349a511bf87ae3da05842346f
Juraj Oršulić <juraj.orsulic@fer.hr>
2018-11-20 15:06:46 +0100

    add copyrights, deduplicate mingw/cygwin cleanup

--- scripts/ctest_to_junit.py
+++ scripts/ctest_to_junit.py
@@ -1,3 +1,17 @@
+# Copyright 2018 The Cartographer Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from lxml import etree
 import StringIO
 import sys
@@ -6,7 +20,7 @@ TAGfile = open(sys.argv[1]+"/Testing/TAG", 'r')
 dirname = TAGfile.readline().strip()
 
 xmlfile = open(sys.argv[1]+"/Testing/"+dirname+"/Test.xml", 'r')
-xslfile = open(sys.path[0] + "/CTest2JUnit.xsl", 'r')
+xslfile = open(sys.path[0] + "ctest_to_junit.xsl", 'r')
 
 xmlcontent = xmlfile.read()
 xslcontent = xslfile.read()
