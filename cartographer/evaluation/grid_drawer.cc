661d5e7aeede953f0c8fb540c9e05056f80863e5
Kevin_Daun <daun@sim.informatik.tu-darmstadt.de>
2019-12-11 19:14:58 +0100

    Update drawing and evaluation tool.

--- cartographer/evaluation/grid_drawer.cc
+++ cartographer/evaluation/grid_drawer.cc
@@ -5,11 +5,18 @@ namespace cartographer {
 namespace evaluation {
 namespace {}  // namespace
 
-GridDrawer::GridDrawer() : scale_(6.0) {
-  int scaled_num_x_cells = 100 * scale_;
-  int scaled_num_y_cells = 100 * scale_;
+GridDrawer::GridDrawer() : scale_(4.0) {
+  min_x_ = -20.f;
+  min_y_ = -1.f;
+  max_x_ = 20.f;
+  max_y_ = 1.f;
+  z_ = 0.0f;
+  max_tsd_ = 0.3f;
+  resolution_ = 0.05f;
+  scaled_num_x_cells_ = scale_ * (max_x_ - min_x_) / resolution_;
+  scaled_num_y_cells_ = scale_ * (max_y_ - min_y_) / resolution_;
   grid_surface_ = cairo_image_surface_create(
-      CAIRO_FORMAT_ARGB32, scaled_num_x_cells, scaled_num_y_cells);
+      CAIRO_FORMAT_ARGB32, scaled_num_x_cells_, scaled_num_y_cells_);
   cairo_pattern_t* pattern = cairo_pattern_create_for_surface(grid_surface_);
   //  cairo_pattern_set_filter (pattern, CAIRO_FILTER_NEAREST);
 
@@ -18,126 +25,124 @@ GridDrawer::GridDrawer() : scale_(6.0) {
   //
   //  cairo_pattern_set_filter (cairo_get_source (grid_surface_context_),
   //  CAIRO_FILTER_NEAREST);
-  cairo_device_to_user_distance(grid_surface_context_, &scale_, &scale_);
+  //  cairo_device_to_user_distance(grid_surface_context_, &scale_, &scale_);
   cairo_set_source_rgba(grid_surface_context_, 1, 1, 1, 1);
   cairo_paint(grid_surface_context_);
 }
 
 void GridDrawer::DrawTSD(const cartographer::mapping::HybridGridTSDF& grid) {
-  //  int scaled_num_x_cells = limits_.cell_limits().num_y_cells;
-  //  int scaled_num_y_cells = limits_.cell_limits().num_x_cells;
-  //  for (int ix = 0; ix < scaled_num_x_cells; ++ix) {
-  //    for (int iy = 0; iy < scaled_num_y_cells; ++iy) {
-  //      float r = 1.f;
-  //      float g = 1.f;
-  //      float b = 1.f;
-  //      float val = grid.GetTSD({iy, ix}) / grid.GetMaxCorrespondenceCost();
-  //      if (val > 0.f) {
-  //        g = 1. - std::pow(std::abs(val), 0.5);
-  //        b = g;
-  //      } else {
-  //        r = 1. - std::pow(std::abs(val), 0.5);
-  //        g = r;
-  //      }
-  //      if (std::abs(val) > 0.999) {
-  //        r = 0.95f;
-  //        g = 0.95f;
-  //        b = 0.95f;
-  //      }
-  //      //      r = 0.2 + 0.6* std::abs(val);
-  //      //      g = 0.2 + 0.6* std::abs(val);
-  //      //      b = 0.2 + 0.6* std::abs(val);
-  //      cairo_set_source_rgb(grid_surface_context_, r, g, b);
-  //      //      cairo_pattern_set_filter (cairo_get_source
-  //      //      (grid_surface_context_), CAIRO_FILTER_NEAREST);
-  //      cairo_rectangle(grid_surface_context_, scale_ * (float(ix)),
-  //                      scale_ * ((float)iy), scale_, scale_);
-  //      cairo_fill(grid_surface_context_);
-  //    }
-  //  }
+  Eigen::Array3i zero_index = grid.GetCellIndex({min_x_, min_y_, z_});
+  for (int ix = 0; ix < scaled_num_x_cells_; ++ix) {
+    for (int iy = 0; iy < scaled_num_y_cells_; ++iy) {
+      float r = 1.f;
+      float g = 1.f;
+      float b = 1.f;
+      float normalized_tsdf =
+          grid.GetTSD(zero_index + Eigen::Array3i({ix, iy, 0})) / max_tsd_;
+      if (normalized_tsdf > 0.f) {
+        g = 1. - std::pow(std::abs(normalized_tsdf), 0.7);
+        b = g;
+      } else {
+        r = 1. - std::pow(std::abs(normalized_tsdf), 0.7);
+        g = r;
+      }
+      cairo_set_source_rgb(grid_surface_context_, r, g, b);
+      cairo_rectangle(grid_surface_context_, (float(ix) - 0.5f) * scale_,
+                      ((float)iy - 0.5f) * scale_, scale_, scale_);
+      cairo_fill(grid_surface_context_);
+    }
+  }
 }
 
-void GridDrawer::DrawWeights(
+void GridDrawer::DrawInterpolatedTSD(
     const cartographer::mapping::HybridGridTSDF& grid) {
-  //  int scaled_num_x_cells = limits_.cell_limits().num_y_cells * scale_;
-  //  int scaled_num_y_cells = limits_.cell_limits().num_x_cells * scale_;
-  //  for (int ix = 0; ix < scaled_num_x_cells; ++ix) {
-  //    for (int iy = 0; iy < scaled_num_y_cells; ++iy) {
-  //      float r = 1.f;
-  //      float g = 1.f;
-  //      float b = 1.f;
-  //      float val =
-  //          grid.GetWeight({iy, ix}) / grid.value_converter_->getMaxWeight();
-  //      if (val > 0.f) {
-  //        g = 1. - std::pow(std::abs(val), 0.5);
-  //        b = g;
-  //      } else {
-  //        r = 1. - std::pow(std::abs(val), 0.5);
-  //        g = r;
-  //      }
-  //      cairo_set_source_rgb(grid_surface_context_, r, g, b);
-  //      cairo_rectangle(grid_surface_context_, scale_ * (float(ix)),
-  //                      scale_ * ((float)iy), scale_, scale_);
-  //      cairo_fill(grid_surface_context_);
-  //    }
-  //  }
+  mapping::scan_matching::InterpolatedTSDF interpolated_tsdf(grid);
+  for (int ix = 0; ix < scaled_num_x_cells_; ++ix) {
+    for (int iy = 0; iy < scaled_num_y_cells_; ++iy) {
+      float r = 1.f;
+      float g = 1.f;
+      float b = 1.f;
+      float normalized_tsdf =
+          interpolated_tsdf.GetTSD(min_x_ + (max_x_ - min_x_) * double(ix) /
+                                                double(scaled_num_x_cells_),
+                                   min_y_ + (max_y_ - min_y_) * double(iy) /
+                                                double(scaled_num_y_cells_),
+                                   z_) /
+          max_tsd_;
+
+      if (normalized_tsdf > 0.f) {
+        g = 1. - std::pow(std::abs(normalized_tsdf), 0.7);
+        b = g;
+      } else {
+        r = 1. - std::pow(std::abs(normalized_tsdf), 0.7);
+        g = r;
+      }
+      cairo_set_source_rgb(grid_surface_context_, r, g, b);
+      cairo_rectangle(grid_surface_context_, float(ix), (float)iy, scale_,
+                      scale_);
+      cairo_fill(grid_surface_context_);
+    }
+  }
 }
 
-void GridDrawer::DrawScan(
-    const sensor::RangeData& range_data,
-    const cartographer::transform::Rigid2d& initial_transform,
-    const cartographer::transform::Rigid2d& matched_transform) {
-  sensor::RangeData initial_pose_estimate_range_data =
-      cartographer::sensor::TransformRangeData(
-          range_data, transform::Embed3D(initial_transform.cast<float>()));
-  sensor::RangeData matched_range_data =
-      cartographer::sensor::TransformRangeData(
-          range_data, transform::Embed3D(matched_transform.cast<float>()));
+void GridDrawer::DrawSinglePointCostFunction(
+    const cartographer::mapping::HybridGridTSDF& grid,
+    mapping::scan_matching::CeresScanMatcher3D& scan_matcher) {
+  const Eigen::Vector3d target_translation({0.0, 0.0, 0.0});
+  sensor::RangefinderPoint point;
+  point.position = {0.f, 0.f, 0.f};
+  sensor::PointCloud single_point = {point};
+  double cost;
+  std::vector<double> residuals;
+  std::vector<double> jacobians;
+  for (int ix = 0; ix < scaled_num_x_cells_; ++ix) {
+    for (int iy = 0; iy < scaled_num_y_cells_; ++iy) {
+      float r = 1.f;
+      float g = 1.f;
+      float b = 1.f;
+      float x =
+          min_x_ + (max_x_ - min_x_) * double(ix) / double(scaled_num_x_cells_);
+      float y =
+          min_y_ + (max_y_ - min_y_) * double(iy) / double(scaled_num_y_cells_);
 
-  //  double scale_ = 1. / limits_.resolution();
-  //
-  //  // Scan Points
-  //  cairo_set_source_rgb(grid_surface_context_, 0.8, 0.0, 0);
-  //  for (auto& scan : initial_pose_estimate_range_data.returns) {
-  //    float x = scale_ * (limits_.max().x() - scan.position[0]);
-  //    float y = scale_ * (limits_.max().y() - scan.position[1]);
-  //    cairo_rectangle(grid_surface_context_, (x - 0.15) * scale_,
-  //                    (y - 0.15) * scale_, 0.3 * scale_, 0.3 * scale_);
-  //  }
-  //  cairo_fill(grid_surface_context_);
-  //
-  //  cairo_set_source_rgb(grid_surface_context_, 0.0, 0.8, 0);
-  //  for (auto& scan : matched_range_data.returns) {
-  //    float x = scale_ * (limits_.max().x() - scan.position[0]);
-  //    float y = scale_ * (limits_.max().y() - scan.position[1]);
-  //    cairo_rectangle(grid_surface_context_, (x - 0.15) * scale_,
-  //                    (y - 0.15) * scale_, 0.3 * scale_, 0.3 * scale_);
-  //  }
-  cairo_fill(grid_surface_context_);
+      const transform::Rigid3d initial_pose_estimate({x, y, z_},
+                                                     {0.0, 0.0, 0.0, 0.0});
+      scan_matcher.Evaluate(target_translation, initial_pose_estimate,
+                            {{&single_point, &grid}}, &cost, &residuals,
+                            &jacobians);
+
+      double cost_drawing_scale = 10.0;
+      double scaled_cost = cost_drawing_scale * cost;
+
+      g = 1. - std::pow(std::abs(scaled_cost), 0.7);
+      b = g;
+
+      cairo_set_source_rgb(grid_surface_context_, r, g, b);
+      cairo_rectangle(grid_surface_context_, float(ix), float(iy), scale_,
+                      scale_);
+      cairo_fill(grid_surface_context_);
+    }
+  }
 }
-
 void GridDrawer::DrawPointcloud(
     const sensor::PointCloud& range_data,
-    const cartographer::transform::Rigid2d& initial_transform,
-    const cartographer::transform::Rigid2d& matched_transform) {
-  sensor::PointCloud initial_pose_estimate_range_data =
-      cartographer::sensor::TransformPointCloud(
-          range_data, transform::Embed3D(initial_transform.cast<float>()));
+    const cartographer::transform::Rigid3d& transform) {
   sensor::PointCloud matched_range_data =
-      cartographer::sensor::TransformPointCloud(
-          range_data, transform::Embed3D(matched_transform.cast<float>()));
-
-  //  double scale_ = 1. / limits_.resolution();
-
-  // Scan Points
-  //
-  //  cairo_set_source_rgb(grid_surface_context_, 0.0, 0.0, 0);
-  //  for (auto& scan : matched_range_data) {
-  //    float x = scale_ * (limits_.max().x() - scan.position[0]);
-  //    float y = scale_ * (limits_.max().y() - scan.position[1]);
-  //    cairo_rectangle(grid_surface_context_, (x - 0.125) * scale_,
-  //                    (y - 0.125) * scale_, 0.25 * scale_, 0.25 * scale_);
-  //  }
+      cartographer::sensor::TransformPointCloud(range_data,
+                                                transform.cast<float>());
+  cairo_set_source_rgb(grid_surface_context_, 0.0, 0.0, 0);
+  float z_range = 0.6;
+  for (auto& scan : matched_range_data) {
+    if (std::abs(scan.position[2] - z_) < z_range) {
+      float x =
+          (scan.position[0] - min_x_) * scaled_num_x_cells_ / (max_x_ - min_x_);
+      float y =
+          (scan.position[1] - min_y_) * scaled_num_y_cells_ / (max_y_ - min_y_);
+      float point_size = 1.5f * scale_;
+      cairo_rectangle(grid_surface_context_, (x - 0.5 * point_size),
+                      (y - 0.5 * point_size), point_size, point_size);
+    }
+  }
   cairo_fill(grid_surface_context_);
 }
 
--- cartographer/evaluation/grid_drawer.h
+++ cartographer/evaluation/grid_drawer.h
@@ -1,8 +1,10 @@
 #ifndef CARTOGRAPHER_EVALUATION_GRID_DRAWER_H
 #define CARTOGRAPHER_EVALUATION_GRID_DRAWER_H
 
-#include <cartographer/mapping/3d/hybrid_grid_tsdf.h>
-#include <cartographer/sensor/point_cloud.h>
+#include "cartographer/mapping/3d/hybrid_grid_tsdf.h"
+#include "cartographer/mapping/internal/3d/scan_matching/ceres_scan_matcher_3d.h"
+#include "cartographer/mapping/internal/3d/scan_matching/interpolated_tsdf.h"
+#include "cartographer/sensor/point_cloud.h"
 #include "cartographer/sensor/range_data.h"
 
 #include "cairo/cairo-svg.h"
@@ -14,17 +16,23 @@ namespace evaluation {
 class GridDrawer {
  public:
   GridDrawer();
+
   void DrawTSD(const cartographer::mapping::HybridGridTSDF& grid);
-  void DrawWeights(const cartographer::mapping::HybridGridTSDF& grid);
 
-  void DrawScan(const sensor::RangeData& range_data,
-                const cartographer::transform::Rigid2d& initial_transform,
-                const cartographer::transform::Rigid2d& matched_transform);
+  void DrawInterpolatedTSD(const cartographer::mapping::HybridGridTSDF& grid);
 
-  void DrawPointcloud(
-      const sensor::PointCloud& range_data,
-      const cartographer::transform::Rigid2d& initial_transform,
-      const cartographer::transform::Rigid2d& matched_transform);
+  void DrawSinglePointCostFunction(
+      const cartographer::mapping::HybridGridTSDF& grid,
+      mapping::scan_matching::CeresScanMatcher3D& scan_matcher);
+
+  //  void DrawWeights(const cartographer::mapping::HybridGridTSDF& grid);
+  //
+  //  void DrawScan(const sensor::RangeData& range_data,
+  //                const cartographer::transform::Rigid2d& initial_transform,
+  //                const cartographer::transform::Rigid2d& matched_transform);
+
+  void DrawPointcloud(const sensor::PointCloud& range_data,
+                      const cartographer::transform::Rigid3d& transform);
 
   void ToFile(std::string filename);
 
@@ -32,6 +40,15 @@ class GridDrawer {
   cairo_surface_t* grid_surface_;
   cairo_t* grid_surface_context_;
   double scale_;
+  int scaled_num_x_cells_;
+  int scaled_num_y_cells_;
+  double min_x_;
+  double min_y_;
+  double max_x_;
+  double max_y_;
+  double z_;
+  float max_tsd_;
+  float resolution_;
 };
 
 }  // namespace evaluation
