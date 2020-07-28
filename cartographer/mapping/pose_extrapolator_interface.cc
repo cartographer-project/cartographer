5e8f14033a9315fb8cc672c8e8b58ef3bf2eb4e9
mschworer <mschworer@lyft.com>
2020-07-28 15:14:03 +0200

    add pose extrapolator interface and extrapolator options
    Signed-off-by: mschworer <mschworer@lyft.com>

---
+++ cartographer/mapping/pose_extrapolator_interface.cc
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2018 The Cartographer Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "cartographer/mapping/pose_extrapolator_interface.h"
+
+#include "cartographer/common/time.h"
+#include "cartographer/mapping/pose_extrapolator.h"
+
+namespace cartographer {
+namespace mapping {
+
+namespace {
+
+proto::ConstantVelocityPoseExtrapolatorOptions
+CreateConstantVelocityPoseExtrapolatorOptions(
+    common::LuaParameterDictionary* const parameter_dictionary) {
+  proto::ConstantVelocityPoseExtrapolatorOptions options;
+  options.set_pose_queue_duration(
+      parameter_dictionary->GetDouble("pose_queue_duration"));
+  options.set_imu_gravity_time_constant(
+      parameter_dictionary->GetDouble("imu_gravity_time_constant"));
+  return options;
+}
+
+}
+
+proto::PoseExtrapolatorOptions CreatePoseExtrapolatorOptions(
+    common::LuaParameterDictionary* const parameter_dictionary) {
+  proto::PoseExtrapolatorOptions options;
+  *options.mutable_constant_velocity() =
+      CreateConstantVelocityPoseExtrapolatorOptions(
+          parameter_dictionary->GetDictionary("constant_velocity").get());
+  return options;
+}
+
+std::unique_ptr<PoseExtrapolatorInterface>
+PoseExtrapolatorInterface::CreateWithImuData(
+    const proto::PoseExtrapolatorOptions& options,
+    const std::vector<sensor::ImuData>& imu_data) {
+  CHECK(!imu_data.empty());
+  return PoseExtrapolator::InitializeWithImu(
+        common::FromSeconds(options.constant_velocity().pose_queue_duration()),
+        options.constant_velocity().imu_gravity_time_constant(),
+        imu_data.back());
+}
+
+}
+}
---
+++ cartographer/mapping/pose_extrapolator_interface.h
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2018 The Cartographer Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef CARTOGRAPHER_MAPPING_POSE_EXTRAPOLATOR_INTERFACE_H_
+#define CARTOGRAPHER_MAPPING_POSE_EXTRAPOLATOR_INTERFACE_H_
+
+#include <memory>
+#include <tuple>
+
+#include "cartographer/common/time.h"
+#include "cartographer/mapping/proto/pose_extrapolator_options.pb.h"
+#include "cartographer/sensor/imu_data.h"
+#include "cartographer/sensor/odometry_data.h"
+#include "cartographer/transform/rigid_transform.h"
+#include "cartographer/transform/timestamped_transform.h"
+
+namespace cartographer {
+namespace mapping {
+
+proto::PoseExtrapolatorOptions CreatePoseExtrapolatorOptions(
+    common::LuaParameterDictionary* const parameter_dictionary);
+
+class PoseExtrapolatorInterface {
+ public:
+  struct ExtrapolationResult {
+    // The poses for the requested times at index 0 to N-1.
+    std::vector<transform::Rigid3f> previous_poses;
+    // The pose for the requested time at index N.
+    transform::Rigid3d current_pose;
+    Eigen::Vector3d current_velocity;
+    Eigen::Quaterniond gravity_from_tracking;
+  };
+
+  PoseExtrapolatorInterface(const PoseExtrapolatorInterface&) = delete;
+  PoseExtrapolatorInterface& operator=(const PoseExtrapolatorInterface&) = delete;
+  virtual ~PoseExtrapolatorInterface() {}
+
+  // TODO: Remove dependency cycle.
+  static std::unique_ptr<PoseExtrapolatorInterface> CreateWithImuData(
+      const proto::PoseExtrapolatorOptions& options,
+      const std::vector<sensor::ImuData>& imu_data);
+
+  // Returns the time of the last added pose or Time::min() if no pose was added
+  // yet.
+  virtual common::Time GetLastPoseTime() const = 0;
+  virtual common::Time GetLastExtrapolatedTime() const = 0;
+
+  virtual void AddPose(common::Time time, const transform::Rigid3d& pose) = 0;
+  virtual void AddImuData(const sensor::ImuData& imu_data) = 0;
+  virtual void AddOdometryData(const sensor::OdometryData& odometry_data) = 0;
+  virtual transform::Rigid3d ExtrapolatePose(common::Time time) = 0;
+
+  virtual ExtrapolationResult ExtrapolatePosesWithGravity(
+      const std::vector<common::Time>& times) = 0;
+
+  // Returns the current gravity alignment estimate as a rotation from
+  // the tracking frame into a gravity aligned frame.
+  virtual Eigen::Quaterniond EstimateGravityOrientation(common::Time time) = 0;
+
+ protected:
+  PoseExtrapolatorInterface() {}
+};
+
+}  // namespace mapping
+}  // namespace cartographer
+
+#endif  // CARTOGRAPHER_MAPPING_POSE_EXTRAPOLATOR_INTERFACE_H_
